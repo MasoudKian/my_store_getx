73ebce1954dd1b542569814a0c32401e0eac01d1
Masoud Kian <masoudkiannezhad@gmail.com>
2023-07-03 14:21:01 +0330

    Initial commit -- Start Project

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyStoreGetX());
+}
+
+class MyStoreGetX extends StatelessWidget {
+  const MyStoreGetX({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
+
+
